d4a7b03ef8f8b92937e8090577a4e2e18a2ad50c
vitmax1 <2california@mail.ru>
2023-12-08 22:24:08 +0600

    counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import './index.scss';
+import React from 'react';
 
 function App() {
+	const [count, setCount] = React.useState(0);
+
+	const onClickPlus = () => {
+		setCount(count + 1);
+	}
+	const onClickMinus = () => {
+		setCount(count - 1);
+	}
+
   return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+      <div className="App">
+          <div>
+              <h2>Счетчик:</h2>
+              <h1>{count}</h1>
+              <button className="minus" onClick={onClickMinus}>
+                  - Минус
+              </button>
+              <button className="plus" onClick={onClickPlus}>
+                  Плюс +
+              </button>
+          </div>
       </div>
-    </div>
   );
 }
 
